be425e6be071fa4b56aab3e037552bad0e3c7c9a
ed633 <edendecolli@icloud.com>
2018-10-12 10:39:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+import java.util.Random;
+import java.lang.Math;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner kbInput1 = new Scanner(System.in);
+        System.out.println("What is your name");
+        String nombre= kbInput1.next();
+        number n1 = new number(nombre, 0);
+        System.out.println("Nice to meet you " + nombre + "! Your lucky number is " + number.highestRandom);
+    }
+}
---
+++ src/number.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Random;
+import java.lang.Math;
+/**
+ * Created by gr236 on 10/9/18.
+ */
+public class number {
+    int max = 100;
+    int min= 0;
+    int range = max - min;
+    public String name;
+    public double random;
+    public static double highestRandom = 0;
+    public number(String n, double r) {
+        name = n;
+        random = r;
+        rando();
+    }
+    private void rando() {
+        if(random == highestRandom) {
+            highestRandom =  (int)(Math.sqrt((Math.random() * range) + min));
+        }
+    }
+}
